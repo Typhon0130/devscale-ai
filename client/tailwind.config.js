baaba9d0b745ead786f2541f4c41a31bd7fd7ba8
Typhon <crystalmaster0130@gmail.com>
2023-07-26 03:39:56 +0900

    initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      desktop: { max: '1440px' },
+      basic: { max: '1280px' },
+      laptop: { max: '1024px' },
+      tablet: { max: '768px' },
+      mobile: { max: '425px' }
+    },
+    extend: {}
+  },
+  plugins: []
+};
