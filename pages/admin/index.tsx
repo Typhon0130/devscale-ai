c398a6f27a7e3c6192f7a934bfe122190567c05a
Typhon <crystalmaster0130@gmail.com>
2023-08-19 03:29:11 +0900

    fix: admin page updated

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -14,7 +14,7 @@ const Admin = () => {
       </div>
       <div className="flex w-full flex-1 flex-col items-center justify-center gap-5">
         <div className="whitespace- flex w-[95%] max-w-[1200px] items-center justify-center break-all	bg-[#444654] py-6 text-center text-sm leading-[24px] text-[#d1d5db]	">
-          https://drive.google.com/drive/folders/10Hdpiw6goGH7gALMsOWcpry-8DxfOJ5A
+          Your Source Folder Url
         </div>
         <button
           className="flex w-[190px] flex-shrink-0 cursor-pointer select-none items-center justify-center gap-3 rounded-sm border border-white/20 bg-primary px-3 py-4 text-center text-[12.5px] leading-3 text-white transition-colors duration-200 hover:bg-bgPrimary"
